ef8ce180256879c4591dd680137e01062c000f95
Ogola Sospeter <okongo.ogola1@students.jkuat.ac.ke>
2026-06-26 12:05:51 +0300

    ci: trigger redeploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // static export for GitHub Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
